701564ec3fc98463f0a1aba3dedf4341be8e0da1
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-05-19 18:18:55 +0200

    Update index.js

--- Udemy/jsx/src/index.js
+++ Udemy/jsx/src/index.js
@@ -3,6 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a react component
-
+/*
+ a react component is a function or a class
+ that produces HTML to show to the user using JSX
+ and handles feedback from the user using event handlers
+*/
+const App = function() {
+  return <div>Hi there!</div>;
+}
 
 // Take the react component and show it on the screen
