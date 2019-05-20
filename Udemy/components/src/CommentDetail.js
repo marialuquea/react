87bc1248b66adc363d17754de526f15e7f437d9d
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-05-20 10:55:59 +0200

    importing other files

---
+++ Udemy/components/src/CommentDetail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import faker from 'faker';
+
+const CommentDetail = () => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar"><img alt="avatar" src={faker.image.avatar()}/></a>
+      <div className="content">
+        <a href="/" className="author">Maria</a>
+        <div className="metadata"><span className="date">Today at 6pm </span></div>
+        <div className="text">Nice blog post!</div>
+      </div>
+    </div>
+  );
+};
+
+// makes it available in other files
+export default CommentDetail;
--- Udemy/components/src/index.js
+++ Udemy/components/src/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Maria
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6pm </span>
-          </div>
-          <div className="text">
-            Nice blog post!
-          </div>
-        </div>
-      </div>
+      <CommentDetail />
     </div>
+
   );
 };
 
