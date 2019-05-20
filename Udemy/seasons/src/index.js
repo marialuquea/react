d5b47bf04ec5771e3415e4a6a85a73762894f27c
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-05-20 12:47:19 +0200

    getting latitude

--- Udemy/seasons/src/index.js
+++ Udemy/seasons/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return (
-    <div>Hi</div>
-  );
-};
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+  }
+
+  // React says we have to define render!!
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
