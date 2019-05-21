dadc24469225d7a910b08de68c1a3f4e1402a5e6
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-05-21 10:16:22 +0200

    icons

--- Udemy/seasons/src/SeasonDisplay.js
+++ Udemy/seasons/src/SeasonDisplay.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter'; // if lat is more than 0 then summer, then winter
+  }
+  else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+}
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+
+  const text =
+    season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach!';
+
+  const icon =
+    season === 'winter' ? 'snowflake' : 'sun';
+
+  console.log('props.lat: ' + props.lat);
+  console.log('season: ' + season);
+  console.log('text: ' + text);
+
+  //return <div>{season ==='winter' ? 'Burr, it is chilly' : 'Lets hit the beach!'}</div>;
+  
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
+
+
 };
 
 export default SeasonDisplay;
--- Udemy/seasons/src/index.js
+++ Udemy/seasons/src/index.js
@@ -1,48 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component{
-  constructor(props) {
+  /*
+  constructor(props) { // good to do one-time setup
     super(props); //initialise
-
     // this is the only time we do direct assignment
     // to this.state
     // always call setState sometime after this
     this.state = { lat: null, errorMessage: '' };
+  }
+  */
+
+  state = { lat: null, errorMessage: '' }; //equivalent to defining a constructor and initialising
+
+
+  //content visible on screen
+  // good for data-loading
+  componentDidMount() {
+    console.log('My component was rendered to the screen');
 
     // call getPosition and give a callback
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        // we called setstate!!!
-        // dones not occur while the constructor is running
-        // will run at some point in time in the future
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {  // second callback
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
-  //content visible on screen
-  componentDidMount() {
-    console.log('My component was rendered to the screen');
-  }
 
   // sit and wait for updates
+  // good to do more data-loading when state/props change
   componentDidUpdate() {
     console.log('My component was just updated - it re-rendered!');
   }
 
+
   // React says we have to define render!!
-  // must be called render and return some amount of js
+  // must be called render and return some amount of jsx
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Lat: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <div>Loading!</div>
