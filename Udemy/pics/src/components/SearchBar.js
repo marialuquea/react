36ce9a991ace34b6760b3f47276fb8bdb5add159
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-05-21 11:40:53 +0200

    solving this problem

--- Udemy/pics/src/components/SearchBar.js
+++ Udemy/pics/src/components/SearchBar.js
@@ -5,15 +5,22 @@ class SearchBar extends React.Component {
   state = { term: ''};
   // if term isn't empty, the text will show when the page renders
 
+  onFormSubmit = (event) => {
+    event.preventDefault(); // so the page doesn't refresh every time the user searches something
+    // arrow functions automatically bind the value of this for all the code
+    // inside the function
+    console.log(this.state.term);
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
-              onChange={(e) => this.setState({ term: e.target.value.toUpperCase() })}
+              onChange={(e) => this.setState({ term: e.target.value })}
               onClick={(event) => console.log('Input was clicked')}
               value = {this.state.term}
             />
