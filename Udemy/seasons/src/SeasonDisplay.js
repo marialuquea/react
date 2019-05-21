6ea1226ede02669d472b9dfa24689c9b0d51be08
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-05-21 10:19:41 +0200

    code improvement

--- Udemy/seasons/src/SeasonDisplay.js
+++ Udemy/seasons/src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+  summer: {
+    text: 'Let\'s hit the becah!',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr it is cold!',
+    iconName: 'snowflake'
+  }
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'summer' : 'winter'; // if lat is more than 0 then summer, then winter
@@ -11,24 +22,22 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season] // { text, iconName }
 
-  const text =
-    season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach!';
-
-  const icon =
-    season === 'winter' ? 'snowflake' : 'sun';
+  //const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach!';
+  //const icon = season === 'winter' ? 'snowflake' : 'sun';
 
   console.log('props.lat: ' + props.lat);
   console.log('season: ' + season);
   console.log('text: ' + text);
 
   //return <div>{season ==='winter' ? 'Burr, it is chilly' : 'Lets hit the beach!'}</div>;
-  
+
   return (
     <div>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
     </div>
   );
 
