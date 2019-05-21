27e04c2210cec1ff4aa1b8c689b0a741e55c070b
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-05-21 12:55:17 +0200

    unsplash token is invalid :(

--- Udemy/pics/src/components/App.js
+++ Udemy/pics/src/components/App.js
@@ -5,10 +5,17 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
   onSearchSubmit(term) {
     console.log(term);
-
-    axios.get('https://api.unsplash.com/search/photos', {
-      
-    });
+    axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        header: {
+          Authorization:
+            'Client-ID 0ad346e10da46f63177b7212ab5ed208a2a0fbda562e5c03783d70e68f946953'
+        }
+      }).
+      then(response => { //callback invoked with whatever data we got from unsplash
+        console.log('response.data.results: ' + response.data.results);  
+      });
   }
 
   render () {
