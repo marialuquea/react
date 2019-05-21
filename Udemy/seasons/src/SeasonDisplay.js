1249798d1568f2cb806bc41367a93c6eeb19f0df
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-05-21 10:38:41 +0200

    loading spinner in page

--- Udemy/seasons/src/SeasonDisplay.js
+++ Udemy/seasons/src/SeasonDisplay.js
@@ -1,8 +1,9 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
 const seasonConfig = {
   summer: {
-    text: 'Let\'s hit the becah!',
+    text: 'Let\'s hit the beach!',
     iconName: 'sun'
   },
   winter: {
@@ -29,10 +30,10 @@ const SeasonDisplay = (props) => {
   console.log('text: ' + text);
 
   return (
-    <div>
-      <i className={`${iconName} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${iconName} icon`} />
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
   );
 
