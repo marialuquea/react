962f5bb822b6ee53a01bb9471b95d05293a9755d
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-05-21 11:03:40 +0200

    start of searchbar

---
+++ Udemy/pics/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const App = () => {
+  return (
+    <div>
+      <h1><SearchBar /></h1>
+    </div>
+  );
+};
+
+export default App;
