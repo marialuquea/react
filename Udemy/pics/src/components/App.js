5e3a50ed66c93ac54874a84df05fc6013ad5dcef
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-05-21 12:25:21 +0200

    added axios

--- Udemy/pics/src/components/App.js
+++ Udemy/pics/src/components/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '10px' }}>
-      <h1><SearchBar /></h1>
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+
+    axios.get('https://api.unsplash.com/search/photos', {
+      
+    });
+  }
+
+  render () {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- Udemy/pics/src/components/SearchBar.js
+++ Udemy/pics/src/components/SearchBar.js
@@ -5,11 +5,15 @@ class SearchBar extends React.Component {
   state = { term: ''};
   // if term isn't empty, the text will show when the page renders
 
-  onFormSubmit = (event) => {
+  onFormSubmit = event => {
     event.preventDefault(); // so the page doesn't refresh every time the user searches something
     // arrow functions automatically bind the value of this for all the code
     // inside the function
-    console.log(this.state.term);
+    console.log('this.state.term: ' + this.state.term);
+
+    this.props.onSubmit(this.state.term);
+
+    console.log('this.props.onSubmit: ' + this.props.onSubmit);
   }
 
   render() {
