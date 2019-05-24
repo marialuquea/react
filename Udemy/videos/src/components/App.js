2c47f67898fbec83881efa4627c97333f20b8c0e
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-05-24 09:49:59 +0200

    videoitem

--- Udemy/videos/src/components/App.js
+++ Udemy/videos/src/components/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
     console.log(term);
@@ -21,11 +22,20 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = (video) => {
+    //console.log('From app: ', video);
+    this.setState({ selectedVideo: video });
+  };
+
   render () {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
