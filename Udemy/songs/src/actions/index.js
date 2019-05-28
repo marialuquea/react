202a21c848e276ebb805fb50774a810447fc3137
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-05-28 12:10:10 +0200

    finished video 143

--- Udemy/songs/src/actions/index.js
+++ Udemy/songs/src/actions/index.js
@@ -1,1 +1,9 @@
-actions
+// Action creator
+
+export const selectSong = (song) => {
+  // Return an action
+  return {
+    type: 'SONG_SELECTED',
+    payload: song
+  };
+};
--- Udemy/songs/src/components/App.js
+++ Udemy/songs/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+//import { selectSong } from '../actions';  // with curly braces bc its not export default
 
 const App = () => {
   return <div>App</div>
